814dd2615616c2e9c36c38a5fea0e8b1dc95723b
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-11-30 21:46:08 +1100

    Hello world webapp

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM sebglazebrook/rust-nightly
+
+EXPOSE 3000
+
+WORKDIR /code
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World")))
+    }).http("0.0.0.0:3000").unwrap();
+}
